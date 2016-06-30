fb85eba02c467fd40786661457626eaa88b1b941
Apoorva Singh <apoorvasingh157@gmail.com>
2016-06-30 20:28:05 +0530

    Basic Shelve Viewer
    Shelve Viewer can now simply print the data inside a shelve file.

--- shelveViewer.py
+++ shelveViewer.py
@@ -1,4 +1,23 @@
 # Python module to view the data inside a shelve
+# Usage : 
+# Command : python shelveViewer.py testing.graph
+# Pass the path of the file as an argument to this script.
+
 
 import shelve
+import sys
+import os
 
+filename = sys.argv[1]
+
+if os.path.exists(filename):
+    print "[*] File Exists : ",filename
+    print "[*] Opening file"
+    f = shelve.open(filename)
+    print "\nShelve data : "
+    for x in f:
+        print x," -> ",f[x]
+    print "\nTotal no. of items in shelve : ",len(f)
+
+else:
+    print "File does not exist : ",filename
